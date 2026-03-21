778127b5b2163953694508fa07e869c6e37c0e09
vladg <gricenkovlad007@gmail.com>
2026-03-21 14:34:09 +0300

    finish

--- class_stack/stack.py
+++ class_stack/stack.py
@@ -15,3 +15,38 @@ class Stack():
             return self._stack.pop()
         except Exception:
             raise StackIsEmpty()
+
+    def __len__(self):
+        return len(self._stack)
+
+    def __str__(self):
+        if not self._stack:
+            return "Stack()"
+        _s = ""
+        for _el in self._stack[:-1]:
+            _s += f"{_el}, "
+        _s += str(self._stack[-1])
+        return f"Stack({_s})"
+
+    def __repr__(self):
+        return f"Stack({self._stack})"
+
+    def __iter__(self):
+        for elem in self._stack:
+            yield elem
+
+    def __contains__(self, value):
+        return value in self._stack
+
+    def __getitem__(self, index):
+        return self._stack[index]
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._stack.clear()
+        return None
+
+    def __eq__(self, other):
+        return self._stack == other._stack
